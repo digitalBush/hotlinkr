1cc80fdbf3105d5c9c92cd7a68afec534b1ba826
Josh Bush <josh@digitalbush.com>
2009-12-31 12:42:40 -0600

    first stab

---
+++ src/hotlinkr.js
@@ -0,0 +1,72 @@
+/* 
+	hotlinkr 
+	Version: 0.1
+	Copyright 2009 Josh Bush (digitalbush.com)	
+	
+	If you are reading this because you were hotlinking to a script hoted on another site, then shame on you.  
+	Please go get the script you need and host it from your own server using you own bandwidth.
+	
+	If you're not sure why what you're doing is wrong, please visit http://altlab.com/hotlinking.html
+*/
+
+(function(type,url) {
+	var body=null;
+
+	function doTags(tag,fn){
+		var elms=document.getElementsByTagName(tag);
+		for (var i = -1, len = elms.length; ++i < len; )
+			fn.call(elms[i]);		
+	}
+		
+	var types={
+		alert:function(){alert("Hotlinking is bad.  Please stop hotlinking to "+url);},
+		redirect: function(){window.location.href="http://"+url;},
+		link:function(){doTags("a",function(){this.href="http://"+url;});},
+		hide:function(){body.style.display="none";},
+		banner:function(){
+			var banner=document.createElement("div");
+			banner.setAttribute("style","position:absolute;top:0;left:0;width:100%;height:100px;border:1px solid #f00;line-height:100px;text-align:center;color:#f00;background:#fff");
+			banner.innerText="Script Hosting provided courtesy of "+url+"<br/>To remove this message, please host scripts from "+url+" on your own server.";		
+			body.appendChild(banner);
+			body.style.paddingTop="100px";
+		},
+		cornify:function(){
+			var s=document.createElement('script');
+			s.src='http://cornify.com/js/cornify.js';
+			body.appendChild(s);
+			
+			setInterval(function(){cornify_add();},5000);
+		},
+		translate:function(){
+			window.location.href="http://translate.google.com/translate?tl=sw&u="+window.location.href;
+		},
+		pad:function(){doTags("div",function(){this.style.padding="300px";});},
+		title:function(){document.title=url+" is my daddy!"},
+		hasselhoff:function(){doTags("img",function(){this.src="http://farm1.static.flickr.com/54/127503978_0c1a5a0f4b_t.jpg"})},
+		overlay:function(){
+			var banner=document.createElement("div");
+			banner.setAttribute("style",'position:absolute;top:0;width:100%;height:100%;right:0;z-index:1000;');			
+			body.appendChild(banner);
+		},
+		flip:function(){
+			doTags("img",function(){this.setAttribute('style',this.getAttribute('style')+";-moz-transform: scaleY(-1);-webkit-transform: scaleY(-1);transform: scaleY(-1);filter: FlipV;")});
+		}
+	};
+	
+	if(type=="random"){
+		var arr=[];
+		for(var key in types){
+			if(types.hasOwnProperty(key))
+				arr.push(key);				
+		}		
+		var rand=Math.floor(Math.random() * arr.length);
+		type=arr[rand];				
+	}
+	
+	window.onload=function(){
+		body=document.getElementsByTagName("body")[0];
+		type=type.split(',');
+		for(var i=-1,len=type.length;++i<len;)
+			types[type[i]]();
+	};
+})("random","digitalbush.com");
