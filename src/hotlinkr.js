4d0288a366b81d6827e0ae20c48aae016969e944
Jared Barboza <jared.m.barboza@gmail.com>
2010-01-05 14:41:48 +0800

    added fail(), spaz(), and resize() types. optimized how functions are chosen and changed code to use addEventListener and attachEvent instead of onload.
    Signed-off-by: Jared Barboza <jared.m.barboza@gmail.com>

--- src/hotlinkr.js
+++ src/hotlinkr.js
@@ -18,6 +18,22 @@
 		for (var i = -1, len = elms.length; ++i < len; )
 			fn.call(elms[i]);		
 	}
+	
+	var isIE=(/msie/i.test(navigator.userAgent) && !/opera/i.test(navigator.userAgent) );
+	
+	function chooseRandomly(obj,seed) {
+		var index = -1, item = null;
+		if( !seed ) {
+			seed=[];
+			for(var key in obj){
+				if(obj.hasOwnProperty(key))
+					seed.push(key);				
+			}		
+		}
+		index=Math.floor(Math.random() * seed.length);
+		if(console && console.log)console.log("hotlinkr >> type = " + seed[index]);
+		return obj[seed[index]];
+	}
 		
 	var types={
 		alert:function(){alert("Hotlinking is bad.  Please stop hotlinking to "+url);},
@@ -45,29 +61,42 @@
 		title:function(){document.title=url+" is my daddy!"},
 		hasselhoff:function(){doTags("img",function(){this.src="http://farm1.static.flickr.com/54/127503978_0c1a5a0f4b_t.jpg"})},
 		overlay:function(){
-			var banner=document.createElement("div");
-			banner.setAttribute("style",'position:absolute;top:0;width:100%;height:100%;right:0;z-index:1000;');			
+			var banner=document.createElement("img");
+			banner.setAttribute("style",'position:absolute;top:0;width:100%;height:' + window.outerHeight + 'px;right:0;z-index:1000;');			
 			body.appendChild(banner);
 		},
 		flip:function(){
-			doTags("img",function(){this.setAttribute('style',this.getAttribute('style')+";-moz-transform: scaleY(-1);-webkit-transform: scaleY(-1);transform: scaleY(-1);filter: FlipV;")});
+			if( !isIE ) {
+				body.setAttribute('style',body.getAttribute('style') + "; -moz-transform: scaleY(-1);-webkit-transform: scaleY(-1);transform: scaleY(-1);filter: FlipV;");
+			} else {
+				doTags("img",function() {
+					this.setAttribute('style', this.getAttribute('style') + "; -moz-transform: scaleY(-1);-webkit-transform: scaleY(-1);transform: scaleY(-1);filter: FlipV;");
+				});
+			}
+		},
+		fail:function() {
+			window.location.href = "http://farm4.static.flickr.com/3102/2780516431_28b7d23cfc.jpg";
 		}
+		
 	};
 	
-	if(type=="random"){
-		var arr=[];
-		for(var key in types){
-			if(types.hasOwnProperty(key))
-				arr.push(key);				
-		}		
-		var rand=Math.floor(Math.random() * arr.length);
-		type=arr[rand];				
+	if( isIE ) {
+		types.resize=function(x,y) {
+			if(x && y) {
+				window.resizeBy(x,y);
+			}else {
+				var local = types, v = new Date().getUTCMilliseconds()%2 == 0 ? -1 : 1;
+				setInterval( function() { try{local.resize(v,v);}catch(e){} }, 10);
+			}
+		};
+		types.spaz=function() {
+			var local = types;
+			setInterval( function() { var v = new Date().getUTCMilliseconds()%2 == 0 ? -1 : 1; try{local.resize(v,v);}catch(e){} }, 1);
+		};
 	}
 	
-	window.onload=function(){
-		body=document.getElementsByTagName("body")[0];
-		type=type.split(',');
-		for(var i=-1,len=type.length;++i<len;)
-			types[type[i]]();
-	};
+	(window.addEventListener ? window.addEventListener : window.attachEvent)(isIE()?"onload":"load",function(){
+		body=document.body?document.body:document.getElementsByTagName("body")[0];
+		(chooseRandomly( types, type === "random" ? undefined : type.split(',') ))();
+	}, false);
 })("random","digitalbush.com");
